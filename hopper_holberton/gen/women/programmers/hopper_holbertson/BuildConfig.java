9bd3848735b79e0a3ce85ad7100e08ec19d8c632
Anne Troop <anne.troop@gmail.com>
2014-11-08 21:03:47 -0500

    hopper_holberton almost works
    except title doesn't show up on Portrait mode

---
+++ hopper_holberton/gen/women/programmers/hopper_holbertson/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package women.programmers.hopper_holbertson;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
