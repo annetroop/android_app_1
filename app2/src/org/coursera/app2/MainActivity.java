d5a52b00c7205809b417d0f834013af0f568a015
Anne Troop <anne.troop@gmail.com>
2014-11-09 20:01:03 -0500

    Added my "app2"
    It works but with very plain strings

---
+++ app2/src/org/coursera/app2/MainActivity.java
@@ -0,0 +1,45 @@
+package org.coursera.app2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void sayHi(android.view.View view) {
+        System.out.println("Hi!");
+	}
+	
+		public void onClick(View view) {
+			Intent intent = new Intent(MainActivity.this, screen2.class);
+            startActivity(intent);
+
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ app2/src/org/coursera/app2/screen2.java
@@ -0,0 +1,13 @@
+package org.coursera.app2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class screen2 extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.secondscreen);
+	}
+	
+}
